476fec19f9368eb6394f9eb09427afb9b34f99d3
unkbitz <149374477+unkbitz@users.noreply.github.com>
2024-09-03 11:13:51 +0200

    starting

---
+++ projects/SarasExample/code/main.cc
@@ -0,0 +1,23 @@
+//------------------------------------------------------------------------------
+// main.cc
+// (C) 2015-2022 Individual contributors, see AUTHORS file
+//------------------------------------------------------------------------------
+#include "config.h"
+#include "exampleapp.h"
+
+//------------------------------------------------------------------------------
+/**
+*/
+int
+main(int argc, const char** argv)
+{
+	Example::ExampleApp app;
+	if (app.Open())
+	{
+		app.Run();
+		app.Close();
+	}
+	app.Exit();	
+
+	return app.ExitCode();
+}
---
+++ projects/SarasExample/code/sarasexampleapp.h
@@ -0,0 +1,36 @@
+#pragma once
+//------------------------------------------------------------------------------
+/**
+	Application class used for example application.
+	
+	(C) 2015-2022 Individual contributors, see AUTHORS file
+*/
+//------------------------------------------------------------------------------
+#include "core/app.h"
+#include "render/window.h"
+
+namespace Example
+{
+class ExampleApp : public Core::App
+{
+public:
+	/// constructor
+	ExampleApp();
+	/// destructor
+	~ExampleApp();
+
+	/// open app
+	bool Open();
+	/// close app
+	void Close();
+	/// run app
+	void Run();
+private:
+
+	GLuint program;
+	GLuint vertexShader;
+	GLuint pixelShader;
+	GLuint triangle;
+	Display::Window* window;
+};
+} // namespace Example
